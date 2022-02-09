c2e637f73ea31e48c11371a13fb126e93d59b797
Rorafa21 <rodrigorafa71@gmail.com>
2022-02-09 00:35:03 -0300

    test

--- teste.py
+++ teste.py
@@ -1,10 +1,11 @@
 import PySimpleGUI as sg      
 
-layout = [[sg.Text('My one-shot window.')],      
-                 [sg.InputText()],      
-                 [sg.Submit(), sg.Cancel()]]      
+layout = [[sg.Text('Teste')],      
+                 [sg.Button("7")],      
+                 [sg.Button("4")],
+                 [sg.Button("1")]]      
 
-window = sg.Window('Window Title', layout)    
+window = sg.Window('Calculadora', layout)    
 
 event, values = window.read()    
 window.close()
