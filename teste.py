32aa18a49aa7d92d8d04e067731767bd64eafeda
Rorafa21 <rodrigorafa71@gmail.com>
2022-02-09 00:42:00 -0300

    font, key

--- teste.py
+++ teste.py
@@ -1,9 +1,15 @@
+from re import S
 import PySimpleGUI as sg      
 
 layout = [[sg.Text('Teste')],      
-                 [sg.Button("7")],      
-                 [sg.Button("4")],
-                 [sg.Button("1")]]      
+                 [sg.Button("7", font=("Consolas", 20), key="-SEVEN-"),      
+                  sg.Button("8"),
+                  sg.Button("9")],
+
+                 [sg.Button("4"),
+                  sg.Button("5"),
+                  sg.Button("6")]]    
+
 
 window = sg.Window('Calculadora', layout)    
 
